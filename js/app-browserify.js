d7d11575f45836596032dd248a3999aae1577255
Rey Thompson <rthompson1@gmail.com>
2015-06-17 22:59:37 -0500

    updated coding, group assignment, in progress

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -2,25 +2,89 @@
 
 // es5 polyfills, powered by es5-shim
 require("es5-shim")
-// es6 polyfills, powered by babel
+    // es6 polyfills, powered by babel
 require("babel/register")
 
+var $ = require('jquery')
+var Backbone = require('backbone')
+var React = require('react')
+
+
 var Promise = require('es6-promise').Promise
-// just Node?
-// var fetch = require('node-fetch')
-// Browserify?
-// require('whatwg-fetch') //--> not a typo, don't store as a var
+var key = "30aa44007c9dbf095d163b5b8a4d6d2e";
+var secret= "9bb09d7d31409e38e956cc8edda544a4";
+// 
+import * as templates from "./templates.js"
+// SC.initialize({
+//   client_id: 'YOUR_CLIENT_ID'
+// });
 
-// other stuff that we don't really use in our own code
-// var Pace = require("../bower_components/pace/pace.js")
+// var track_url = 'http://soundcloud.com/forss/flickermood';
+// SC.oEmbed(track_url, { auto_play: true }, function(oEmbed) {
+//   console.log('oEmbed response: ' + oEmbed);
+// });
+// 
 
-// require your own libraries, too!
-// var Router = require('./app.js')
+var qs = (s, d) => (d || document).querySelector(s)
 
-// window.addEventListener('load', app)
+var SoundcloudCollection = Backbone.Collection.extend({
+    url: () => `https://api.soundcloud.com/tracks.json?client_id=${key}`
+})
 
-// function app() {
-    // start app
-    // new Router()
-// }
 
+
+class SoundcloudItem extends React.Component {
+        constructor(props) {
+            super(props)
+        }
+        render() {
+        var url = this.props.item.get('permalink_url'),
+            artwork_url = this.props.item.get('artwork_url'),
+            title = this.props.item.get('title'),
+            playCount = this.props.item.get('playback_count'),
+            favCount = this.props.item.get('favoritings_count'),
+            img = artwork_url ? (<img src={artwork_url} />) : '';
+
+        return (
+        <div className="item"> 
+                    <div className="play">
+                         {img}
+                     <div className="play-button"> </div>
+                         </div>
+                     <div className="song-title">
+                        {title}
+                    </div> 
+                 <div className ="play-time"> </div>
+                <div className="icons">      
+                     <span className="cloud"> </span>
+                     <span className="buy"> BUY </span>
+                     <span className= "play-number">  {playCount} </span>
+                     <span className="likes"> {playCount} </span>
+                 </div>
+        </div>
+        )
+
+    }
+
+}
+
+class SoundcloudView extends React.Component {
+    constructor(props) {
+        super(props)
+        this.props.items.on('sync', ()=> this.forceUpdate() )
+    }
+    render() {
+        return ( 
+            <div>
+                    <ul> 
+                        {this.props.items.map((i)=> <SoundcloudItem key={i.id} item={i} />)}
+                    </ul>
+            </div>
+        )
+    }
+}
+
+var collection = new SoundcloudCollection();
+React.render( <SoundcloudView title="SoundCloud" items={collection} /> , qs('.container'))
+collection.fetch().then((data) => { 
+        console.log(data); })
---
+++ js/templates.js
@@ -0,0 +1,26 @@
+var $ = require('jquery');
+`<head>
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../dist/style.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+</head>`
+
+export var home = () => { `
+     <div class="container">
+        <div class="item"> 
+        <div class="play"> </div>
+        <div class="song-title"> </div> 
+        <div class ="play-time"> </div>
+        <div class="icons"> 
+        <span class="cloud"> <i class="large mdi-editor-insert-chart"> </i> </span>
+        <span class="buy"> </span>
+        <span class-"play-number"> </span>
+        <span class="likes"> </span>
+        </div>
+        </div>
+    </div>
+   `
+
+}
